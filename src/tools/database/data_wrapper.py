46fb6b36e87ec769ada8b65b5bf21ab55c593b74
guillemalomar <alomarguillem@nebutec.cn>
2019-08-06 10:35:34 +0800

    Modified the data wrappers to have better encapsulation

--- src/tools/database/data_wrapper.py
+++ src/tools/database/data_wrapper.py
@@ -1,24 +1,25 @@
 import sys
 
-from src.tools.database.mongodb_wrapper import MongodbDbWrapper
-from src.tools.database.mysql_wrapper import MysqlDbWrapper
 from src.tools.database.settings import MESSAGES
 
 
 class DataWrapper:
-    def __init__(self, db=None, environment=None):
-        if db.lower() == 'mongodb':
-            self.db = True
-            self.my_data = MongodbDbWrapper()
-        elif db.lower() == 'mysql':
-            self.db = True
-            self.my_data = MysqlDbWrapper(environment)
-        elif db.lower() == 'not defined':
+    def __init__(self, db=None, params=None, environment=None):
+        if db:
+            if db.lower() == 'mongodb':
+                from src.tools.database.mongodb_wrapper import MongodbDbWrapper
+                self.db = True
+                self.my_data = MongodbDbWrapper(params)
+            elif db.lower() == 'mysql':
+                from src.tools.database.mysql_wrapper import MysqlDbWrapper
+                self.db = True
+                self.my_data = MysqlDbWrapper(params, environment)
+            else:
+                print(MESSAGES["WRONG_DATABASE"])
+                sys.exit()
+        else:
             self.db = False
             self.my_data = {}
-        else:
-            print(MESSAGES["WRONG_DATABASE"])
-            sys.exit()
 
     def __setitem__(self, key, value):
         if not self.db and key not in self.my_data:
